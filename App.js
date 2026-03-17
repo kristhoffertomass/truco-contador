1aa0a50ffd7c714190fe7f44efe260aea9b53358
kristhoffertomass <kristhoffer040503@edu.unipar.br>
2026-03-17 20:34:09 -0300

    adiciona botões de pontuação do truco (3,6,9,12)

--- App.js
+++ App.js
@@ -60,3 +60,24 @@ const styles = StyleSheet.create({
     fontSize: 60,
   },
 });
+import { useState } from "react";
+import { Text, View, Button } from "react-native";
+
+export default function App() {
+  const [pontuacao, setPontuacao] = useState(0);
+
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text style={{ fontSize: 24 }}>Marcador</Text>
+      <Text style={{ fontSize: 60 }}>{pontuacao}</Text>
+
+      <Button title="+" onPress={() => setPontuacao(pontuacao + 1)} />
+      <Button
+        title="-"
+        onPress={() =>
+          setPontuacao(pontuacao > 0 ? pontuacao - 1 : 0)
+        }
+      />
+    </View>
+  );
+}
