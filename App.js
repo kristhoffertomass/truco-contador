9ef6754755abf60f75052b81d58c7e9196e97821
kristhoffertomass <kristhoffer040503@edu.unipar.br>
2026-03-17 20:28:53 -0300

    adiciona placar para dois times

--- App.js
+++ App.js
@@ -19,3 +19,44 @@ export default function App() {
     </View>
   );
 }
+import { useState } from "react";
+import { Text, View, Button, StyleSheet } from "react-native";
+
+export default function App() {
+  const [pontuacaoNos, setPontuacaoNos] = useState(0);
+  const [pontuacaoEles, setPontuacaoEles] = useState(0);
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.time}>
+        <Text style={styles.titulo}>Nós</Text>
+        <Text style={styles.numero}>{pontuacaoNos}</Text>
+        <Button title="+" onPress={() => setPontuacaoNos(pontuacaoNos + 1)} />
+      </View>
+
+      <View style={styles.time}>
+        <Text style={styles.titulo}>Eles</Text>
+        <Text style={styles.numero}>{pontuacaoEles}</Text>
+        <Button title="+" onPress={() => setPontuacaoEles(pontuacaoEles + 1)} />
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center",
+  },
+  time: {
+    alignItems: "center",
+  },
+  titulo: {
+    fontSize: 24,
+  },
+  numero: {
+    fontSize: 60,
+  },
+});
